a194698ad11598270d3c24b4414b8f0656ae21dc
Christian Caballero <xtian.cr78@gmail.com>
2024-09-05 09:34:44 +0000

    revisando

---
+++ src/js/component/footers.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Footers() {
+  return (
+    <footer>
+      <p>max chistian</p>
+    </footer>
+  );
+}
+export default Footers;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,11 +5,14 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			
-		</div>
-	);
+  return (
+    <div className="text-center">
+      <Headers />
+      <Jumbotrons />
+      <Cards />
+      <Footers />
+    </div>
+  );
 };
 
 export default Home;
