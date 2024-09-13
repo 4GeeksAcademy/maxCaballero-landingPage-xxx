9dee395ad7b814fd47e2325eeaa916ba95ab3bc3
Christian_Caballero <94983984+xtiancr78@users.noreply.github.com>
2024-09-13 01:38:17 +0000

    agregando card

--- src/js/component/header.jsx
+++ src/js/component/header.jsx
@@ -3,36 +3,34 @@ import React from "react";
 
 
 const Header = () => {
-    return(
+    return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <a className="navbar-brand" href="#">Navbar</a>
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
-  </button>
+            <a className="navbar-brand" href="#">Start Bootstrap</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
 
-  <div className="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul className="navbar-nav mr-auto">
-      <li className="nav-item active">
-        <a className="nav-link" href="#">About <span className="sr-only">(current)</span></a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#">Services</a>
-      </li>
-      
-    </ul>
-    <form className="form-inline my-2 my-lg-0">
-    <ul className="navbar-nav mr-auto">
-      <li className="nav-item active">
-        <a className="nav-link" href="#">About <span className="sr-only">(current)</span></a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#">Services</a>
-      </li>
-      
-    </ul>
-    </form>
-  </div>
-</nav>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav mr-auto">
+                </ul>
+                <form className="form-inline my-2 my-lg-0">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item active">
+                            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                        </li>
+                        <li className="nav-item active">
+                            <a className="nav-link" href="#">About <span className="sr-only">(current)</span></a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Services</a>
+                        </li>
+                        <li className="nav-item active">
+                            <a className="nav-link" href="#">Contacts <span className="sr-only">(current)</span></a>
+                        </li>
+                    </ul>
+                </form>
+            </div>
+        </nav>
     )
 };
 export default Header;
